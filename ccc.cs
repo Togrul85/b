6849f34b08125785c416359c5cd1f1ea6405adc4
unknown <toghrul@code.edu.az>
2022-08-07 23:34:33 +0400

    .

---
+++ ccc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 5, 7, 8, 9 };
+            Console.WriteLine(MinMaX(arr,num));
+        }
+        static void MinMaX (int[] arr,int num)
+        {
+            
+            int mini = 0;
+            int maxi = 0;
+            for (int i = 0; i <num.Length; i++)
+            {
+                if (arr[i]<mini)
+                {
+                    mini = i;
+                }
+
+            }
+            
+        }
+    }
+}
